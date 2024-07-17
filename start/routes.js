8b9ae09163e073986f25e858af7cea509175e0f8
gabrielisantos-dev <gabrielicristina189@gmail.com>
2024-07-17 00:00:52 -0300

    feat: criação das rotas

--- start/routes.js
+++ start/routes.js
@@ -1,21 +1,22 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URLs and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-|
-*/
-
-/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
+Route.post('/signup', 'UserController.signup')
+Route.post('/login', 'UserController.login')
+
+Route.get('/clients', 'ClientController.index')
+Route.get('/clients/:id', 'ClientController.show')
+Route.post('/clients', 'ClientController.store')
+Route.put('/clients/:id', 'ClientController.update')
+Route.delete('/clients/:id', 'ClientController.delete')
+
+Route.get('/products', 'ProductController.index')
+Route.get('/products/:id', 'ProductController.show')
+Route.post('/products', 'ProductController.store')
+Route.put('/products/:id', 'ProductController.update')
+Route.delete('/products/:id', 'ProductController.delete')
+
+Route.post('/sales', 'SaleController.store')
+
+module.exports = Route
